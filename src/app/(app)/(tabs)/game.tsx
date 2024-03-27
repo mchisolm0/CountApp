7abc88bce0f1baa9094e84dcc00c29ef8932fc82
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2024-03-27 16:26:15 -0500

    add 2 placeholder player cards in gamescreen

--- src/app/(app)/(tabs)/game.tsx
+++ src/app/(app)/(tabs)/game.tsx
@@ -1,7 +1,7 @@
 
-import React from "react";
+import React, { ReactElement } from "react";
 import { Image, ImageStyle, TextStyle, View, ViewStyle } from "react-native";
-import { ListItem, Screen, Text } from "src/components";
+import { Button, Card,Header,Icon, ListItem, Screen, Text } from "src/components";
 import { spacing } from "src/theme";
 import { openLinkInBrowser } from "src/utils/openLinkInBrowser";
 import { isRTL } from "src/i18n";
@@ -12,11 +12,101 @@ const reactNativeLiveLogo = require("assets/images/demo/rnl-logo.png");
 const reactNativeRadioLogo = require("assets/images/demo/rnr-logo.png");
 const reactNativeNewsletterLogo = require("assets/images/demo/rnn-logo.png");
 
+
+
 export default function GameScreen() {
-  return (
+    // State variables for life points
+    const [lifePoints, setLifePoints] = React.useState(20);
+
+    // Functions to add and subtract life points
+    const addLifePoints = () => {
+        setLifePoints(lifePoints + 1);
+    };
+    const subtractLifePoints = () => {
+        setLifePoints(lifePoints - 1);
+    };
+    
+    // State variables for life points
+    const [lifePoints1, setLifePoints1] = React.useState(20);
+
+    // Functions to add and subtract life points
+    const addLifePoints1 = () => {
+        setLifePoints1(lifePoints1 + 1);
+    };
+    const subtractLifePoints1 = () => {
+        setLifePoints1(lifePoints1 - 1);
+    }; 
+    return (
     <Screen preset="auto" contentContainerStyle={$container} safeAreaEdges={["top"]}>
       <Text preset="heading" tx="gameScreen.title" style={$title} />
-      
+      <Card
+       horizontalAlignment="center"
+       verticalAlignment="center"
+       style={{ minHeight: 160 }}
+        HeadingComponent={
+          <Text
+            style={{ marginVertical: spacing.sm }}
+            size="xxs"
+            text="Player 1"
+          />
+        }
+       LeftComponent={
+            <Button
+            style={{ marginVertical: spacing.sm }}
+            onPress={subtractLifePoints1}
+            LeftAccessory={(props) => <Icon style={props.style} icon="caretLeft" />}
+          />
+        }
+        ContentComponent={
+          <Text
+            style={{ marginVertical: spacing.sm }}
+            size="xxl"
+            preset="bold"
+            text={lifePoints1.toString()}
+          />
+        }
+        RightComponent={
+            <Button
+            style={{ marginVertical: spacing.sm }}
+            onPress={addLifePoints1}
+            RightAccessory={(props) => <Icon style={props.style} icon="caretRight" />}
+          />
+        }
+      />
+      <Card
+       horizontalAlignment="center"
+       verticalAlignment="center"
+       style={{ minHeight: 160 }}
+       HeadingComponent={
+        <Text
+          style={{ marginVertical: spacing.sm }}
+          size="xxs"
+          text="Player 2"
+        />
+      }
+      LeftComponent={
+            <Button
+            style={{ marginVertical: spacing.sm }}
+            onPress={subtractLifePoints}
+            LeftAccessory={(props) => <Icon style={props.style} icon="caretLeft" />}
+          />
+        }
+        ContentComponent={
+          <Text
+            style={{ marginVertical: spacing.sm }}
+            size="xxl"
+            preset="bold"
+            text={lifePoints.toString()}
+          />
+        }
+        RightComponent={
+            <Button
+            style={{ marginVertical: spacing.sm }}
+            onPress={addLifePoints}
+            RightAccessory={(props) => <Icon style={props.style} icon="caretRight" />}
+          />
+        }
+      />
     </Screen>
   );
 }
