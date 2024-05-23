db0f9a75447ca3f3f3c50bb10e5a0829847b3ca1
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2024-05-23 14:42:56 -0500

    add _layout

---
+++ src/app/_layout.tsx
@@ -0,0 +1,34 @@
+// app/_layout.tsx
+import React from "react";
+import { ViewStyle } from "react-native"
+import { Slot, SplashScreen } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import { useInitialRootStore } from "src/models";
+
+SplashScreen.preventAutoHideAsync();
+
+if (__DEV__) {
+  // Load Reactotron configuration in development. We don't want to
+  // include this in our production bundle, so we are using `if (__DEV__)`
+  // to only execute this in development.
+  require("src/devtools/ReactotronConfig.ts");
+}
+
+export { ErrorBoundary } from "src/components/ErrorBoundary/ErrorBoundary";
+
+export default function Root() {
+  // Wait for stores to load and render our layout inside of it so we have access
+  // to auth info etc
+  const { rehydrated } = useInitialRootStore();
+  if (!rehydrated) {
+    return null;
+  }
+
+  return (
+    <GestureHandlerRootView style={$root}>
+      <Slot />
+    </GestureHandlerRootView>
+  )
+}
+
+const $root: ViewStyle = { flex: 1 }
