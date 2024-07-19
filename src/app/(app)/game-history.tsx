74d842e19b6ef554f73056acd117a0385efd4562
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2024-07-19 11:38:25 -0700

    draft history screen

---
+++ src/app/(app)/game-history.tsx
@@ -0,0 +1,64 @@
+import { observer } from "mobx-react-lite"
+import React from "react"
+import { ImageStyle, TextStyle, ViewStyle } from "react-native"
+import { Card, Screen, Text } from "src/components"
+import { Game, useStores } from "src/models"
+import { spacing } from "src/theme"
+
+// TODO: Replace with actual logos
+// const chainReactLogo = require("assets/images/demo/cr-logo.png")
+// const reactNativeLiveLogo = require("assets/images/demo/rnl-logo.png")
+// const reactNativeRadioLogo = require("assets/images/demo/rnr-logo.png")
+// const reactNativeNewsletterLogo = require("assets/images/demo/rnn-logo.png")
+
+function GameHistoryScreen() {
+  const {
+    gameStore: { games },
+  } = useStores()
+  // useHeader(
+  //   {
+  //     leftIcon: "back",
+  //     onLeftPress: () => router.back(),
+  //     rightText: "endGame",
+  //     onRightPress: endGame,
+  //   },
+  //   [endGame],
+  // )
+
+  return (
+    <Screen
+      preset="fixed"
+      contentContainerStyle={$screenContentContainer}
+      safeAreaEdges={["bottom"]}
+    >
+      {games.map((game: Game) => {
+        return (
+          <Card
+            key={game.gameID}
+            style={$container}
+            ContentComponent={<Text text={"Game " + game.gameID} />}
+          />
+        )
+      })}
+    </Screen>
+  )
+}
+
+export default observer(GameHistoryScreen)
+
+const $container: ViewStyle = {
+  paddingTop: spacing.lg + spacing.xl,
+  paddingHorizontal: spacing.lg,
+}
+const $screenContentContainer: ViewStyle = {
+  flex: 1,
+  paddingVertical: spacing.xxxs,
+  paddingHorizontal: spacing.xxxs,
+}
+
+
+
+
+
+
+
